4921557e4a61fd5bc3174555cfa6d0fa3274cffd
nikhilcs54 <112414218+nikhils31897@users.noreply.github.com>
2023-03-10 22:50:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './Components/Header'
+import Movie from './Components/Movie'
+import datas from './data.json'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className='main'>
+        {
+          datas.map((ele)=>{
+          return(
+            <Movie title={ele.Title} year={ele.Year} img={ele.Poster}/>
+          )
+          
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+       <h1>Movie App</h1> 
+        
+    </div>
+  )
+}
